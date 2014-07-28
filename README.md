fc2d90ee257bf4b1aee2bbbaa5f95fbe17647121
Christo Crampton <info@38.co.za>
2014-07-28 19:53:15 +0200

    add meta/main

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ toast38coza.djangoapp
 =====================
 
 Ansible role fo installing one or more django apps on a server
+
+### Install 
+
+    ansible-galaxy install toast38coza.djangoapp
+    
+
---
+++ templates/local_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.{{item.djangoapp_db_engine}}', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': {{item.djangoapp_projectname}},                      # Or path to database file if using sqlite3.
+        # 'NAME': os.path.join(PROJECT_ROOT, 'data.db'),                      # Or path to database file if using sqlite3.
+        'USER': '{{item.djangoapp_projectname}}',                      # Not used with sqlite3.
+        'PASSWORD': '{{item.djangoapp_projectname}}',                  # Not used with sqlite3.
+        'HOST': '{{item.djangoapp_db_host}}',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
